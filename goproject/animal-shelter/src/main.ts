ead5a0b323e6b88d5dd407c6030cec59baf5c0fd
po133na <antrauxxxxx@mail.ru>
2024-03-02 23:31:15 +0500

    commit

--- goproject/animal-shelter/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import {AnimalListComponent } from './app/app.component';
-
-bootstrapApplication(AnimalListComponent, appConfig)
-  .catch((err) => console.error(err));
